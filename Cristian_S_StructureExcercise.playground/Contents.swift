d49865281733eafe45b0c26b02956bb5b2dbae01
TechyCriswiz <67975057+TechyCriswiz@users.noreply.github.com>
2020-07-27 11:33:59 -0500

    Add files via upload

---
+++ Cristian_S_StructureExcercise.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+struct EngineType {
+    var engine1 = "V4"
+    var engine2 = "V6"
+    var engine3 = "V8"
+}
+
+let engineChoice = EngineType()
+
+print("Your first engine option is \(engineChoice.engine1) \n")
+print("Your second engine option is \(engineChoice.engine2) \n")
+print("Your third engine option is \(engineChoice.engine3) \n")
